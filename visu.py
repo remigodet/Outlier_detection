60a42d48c4fc7386f58aa112b56bcca7135c09b1
remi godet <remi.godet@student-cs.fr>
2022-04-22 00:09:45 +0200

    refactorisation

---
+++ visu.py
@@ -0,0 +1,9 @@
+# tools for testing trained models perfs and creating an optimal outlier detector
+
+# function to be determined ...
+
+# def visu(params)
+#   params are the type of results
+#   plot ?
+
+# def get_detector ?
