70cd4ab615ac209940a83e2d0b198356fc7a7052
Thomas Bouiniere <thomas.bouiniere@student-cs.fr>
2022-04-23 17:27:27 +0200

    data done

--- data.py
+++ data.py
@@ -9,25 +9,69 @@ def data(params):
     # to be used in main.py
     # please test
 
-    # Parameters
-    batch = params['batch_size']
-    outliers = params['outliers']
+    # Parameters default value
+    batch_size = 50
+    outliers = []
+    dataset = 'test'
+    use_negative_index = False
 
-    mnist_testset = datasets.MNIST(
-        root='./data', train=False, download=True, transform=ToTensor()
-    )
+    if 'batch_size' in params:
+        batch_size = params['batch_size']
+    if 'outliers' in params:
+        outliers = params['outliers']
+    if 'dataset' in params:
+        dataset = params['dataset']
+    if 'use_negative' not in params:
+        # If True renvoie le tuple des images à indexs positifs et négatifs
+        use_negative_index = params['use_negative_index']
 
-    indexes = []
-    neg_indexes = []
-    for i in range(len(mnist_testset)):
-        if mnist_testset[i][1] not in outliers:
-            indexes.append(i)
-        else:
-            neg_indexes.append(i)
+    if dataset == 'test':
 
-    mnist_testset_pos = torch.utils.data.Subset(mnist_testset, indexes)
+        mnist_testset = datasets.MNIST(
+            root='./data', train=False, download=True, transform=ToTensor()
+        )
 
-    test_loader = torch.utils.data.DataLoader(
-        mnist_testset_pos, batch_size=batch)
+        indexes = []
+        neg_indexes = []
+        for i in range(len(mnist_testset)):
+            if mnist_testset[i][1] not in outliers:
+                indexes.append(i)
+            else:
+                neg_indexes.append(i)
 
-    return test_loader
+        mnist_testset_pos = torch.utils.data.Subset(mnist_testset, indexes)
+        test_loader_pos = torch.utils.data.DataLoader(
+            mnist_testset_pos, batch_size=batch_size)
+
+        mnist_testset_neg = torch.utils.data.Subset(mnist_testset, neg_indexes)
+        test_loader_neg = torch.utils.data.DataLoader(
+            mnist_testset_neg, batch_size=batch_size)
+
+        if use_negative_index:
+            return test_loader_pos, test_loader_neg
+        return test_loader_pos
+
+    if dataset == 'train':
+        mnist_trainset = datasets.MNIST(
+            root='./data', train=True, download=True, transform=ToTensor())
+
+        indexes = []
+        neg_indexes = []
+        for i in range(len(mnist_trainset)):
+            if mnist_trainset[i][1] not in outliers:
+                indexes.append(i)
+            else:
+                neg_indexes.append(i)
+
+        mnist_trainset_pos = torch.utils.data.Subset(mnist_trainset, indexes)
+        train_loader_pos = torch.utils.data.DataLoader(
+            mnist_trainset_pos, batch_size=batch_size)
+
+        mnist_trainset_neg = torch.utils.data.Subset(
+            mnist_trainset, neg_indexes)
+        train_loader_neg = torch.utils.data.DataLoader(
+            mnist_trainset_neg, batch_size=batch_size)
+
+        if use_negative_index:
+            return train_loader_pos, train_loader_neg
+        return train_loader_pos
