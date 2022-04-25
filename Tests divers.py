1f1c2f9bf80b5dadf1120b433782c554b7a47ab8
Thomas CB <thomas.demontaignedeponcinsvandenbroek@student-cs.fr>
2022-04-25 15:00:11 +0200

    Changements

---
+++ Tests divers.py
@@ -0,0 +1,21 @@
+import visu as visu
+import numpy as np                       
+import torch                       
+import torchvision                       
+from torch import nn                       
+from torch.autograd import Variable                       
+from torchvision.datasets import MNIST                       
+from torchvision.transforms import transforms                       
+from torchvision.utils import save_image
+import matplotlib.pyplot as plt
+from models.AE_Thomas_dMdP import Autoencoder
+
+
+
+
+
+
+
+model = Autoencoder()
+
+model.load('./saved_models/AE_ThomasdMdP.pth')
